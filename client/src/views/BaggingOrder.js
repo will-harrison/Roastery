b44516a650f189a0b1bd8a52401c5fb1536aaae8
Will Harrison <willharrison@gmail.com>
2017-11-29 11:43:22 -0800

    RoastOrder component work

--- client/src/views/BaggingOrder.js
+++ client/src/views/BaggingOrder.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import _ from 'lodash';
 import api from "../api";
 import PageHeader from "../components/PageHeader";
 import OnOrder from '../components/OnOrder';
--- client/src/views/RoastOrder.js
+++ client/src/views/RoastOrder.js
@@ -1,10 +1,32 @@
 import React, { Component } from "react";
+import api from '../api';
+import OnOrder from '../components/OnOrder';
 
 class RoastOrder extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      orders: [],
+      invTypes: ["roastedBulk"]
+    };
+  }
+
+  componentDidMount() {
+    api.inventory.getAll().then(orders => {
+      this.setState(state => {
+        return {
+          ...state,
+          orders
+        };
+      });
+    });
+  }
+
   render() {
+    let { orders, invTypes } = this.state;
     return (
       <div>
-        <h1>RoastOrder</h1>
+        <OnOrder orders={orders} invTypes={invTypes} />
       </div>
     );
   }
