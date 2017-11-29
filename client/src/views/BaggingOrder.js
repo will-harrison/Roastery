029894c1b3cd443f6ee88c3ce346f9ecf2c8568d
Will Harrison <willharrison@gmail.com>
2017-11-29 13:48:55 -0800

    merge

--- client/src/views/BaggingOrder.js
+++ client/src/views/BaggingOrder.js
@@ -28,19 +28,7 @@ class BaggingOrder extends Component {
     return (
       <div>
         <PageHeader>Bag Order</PageHeader>
-<<<<<<< HEAD
-
-        {orders.map(order => {})}
-
-        {orders.map(order => (
-          <div>
-            {order.item.name}
-            <button>Bag</button>
-          </div>
-        ))}
-=======
         <OnOrder orders={orders} invTypes={invTypes} />
->>>>>>> 5d1d4a132756971c520fbe2a6426e2e0ac31280a
       </div>
     );
   }
